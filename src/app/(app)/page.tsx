d1f4ee1166f7029f0a7a92d6accd52635271d603
Frederic <frederic.ferreira@valraiso.fr>
2024-08-18 21:51:03 +0200

    comment before mise en ligne

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,7 +1,7 @@
 import { getPayloadHMR } from '@payloadcms/next/utilities'
 import configPromise from '@payload-config'
-import { IntroductionContent, RichTextContent } from '@/types/rich-text'
-import { ImageContent } from '@/types/images'
+// import { IntroductionContent, RichTextContent } from '@/types/rich-text'
+// import { ImageContent } from '@/types/images'
 import Gallery from '@/components/gallery'
 import Introduction from './components/introduction'
 import ContactButton from '@/components/contact-button'
@@ -9,25 +9,25 @@ import ContactButton from '@/components/contact-button'
 const Page = async () => {
   const payload = await getPayloadHMR({ config: configPromise })
 
-  const [introductionData, imagesData] = await Promise.all([
-    payload.find({
-      collection: 'introduction',
-    }),
-    payload.find({
-      collection: 'images',
-      sort: 'position',
-    }),
-  ])
+  // const [introductionData, imagesData] = await Promise.all([
+  //   payload.find({
+  //     collection: 'introduction',
+  //   }),
+  //   payload.find({
+  //     collection: 'images',
+  //     sort: 'position',
+  //   }),
+  // ])
 
-  const introduction = introductionData?.docs[0] as IntroductionContent
+  // const introduction = introductionData?.docs[0] as IntroductionContent
 
-  const images = imagesData?.docs as ImageContent[]
+  // const images = imagesData?.docs as ImageContent[]
 
   return (
     <main className="index">
-      <Introduction introduction={introduction} />
+      {/* <Introduction introduction={introduction} /> */}
       <h1 className="title">Découvrir mes créations !</h1>
-      <Gallery images={images} />
+      {/* <Gallery images={images} /> */}
       <ContactButton />
     </main>
   )
